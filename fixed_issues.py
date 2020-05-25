9c6afba6adc88aa54671ff4afe2af7da58d499d4
andrijapanicsb <andrija.panic@shapeblue.com>
2020-05-25 13:53:11 +0100

    refractor code

--- fixed_issues.py
+++ fixed_issues.py
@@ -22,7 +22,7 @@ Usage:
   fixed_issues.py [--config=<config.json>]
                   [-t <arg> | --gh_token=<arg>] 
                   [-c <arg> | --prev_rel_commit=<arg>]
-                  [-b <arg> | --branches=<arg>]  
+                  [-b <arg> | --branch=<arg>]  
                   [--repo=<arg>] 
                   [--gh_base_url=<arg>] 
                   [--jira_base_url=<arg>]
@@ -55,12 +55,24 @@ Options:
   --col_type_width=<arg>            The width of the Issue Type column [default: 15].
   --col_priority_width=<arg>        The width of the Issue Priority column [default: 10].
   --col_desc_width=<arg>            The width of the Description column [default: 60].
+  
+Sample json file contents:
+
+{
+    "--gh_token":"******************",
+    "--prev_release_commit":"",
+    "--repo_name":"apache/cloudstack",
+    "--branch":"4.11",
+    "--prev_release_ver":"4.11.1.0",
+    "--new_release_ver":"4.11.2.0"
+}
+
+
 """
 
 import docopt
 import json
 from github import Github
-from jira import JIRA
 from lib.Table import TableRST, TableMD
 import itertools
 import os.path
@@ -80,14 +92,14 @@ def load_config():
         try:
             with open(args['--config']) as json_file:    
                 json_args = json.load(json_file)
-        except Exception, e:
+        except Exception as e:
             print("Failed to load config file '%s'" % args['--config'])
             print("ERROR: %s" % str(e))
         if json_args:
             args = merge(args, json_args)
-    # since we are here, check that the required fields exist
+#     since we are here, check that the required fields exist
     valid_input = True
-    for arg in ['--gh_token', '--prev_rel_commit', '--branches']:
+    for arg in ['--gh_token', '--prev_release_ver', '--branch', '--repo', '--new_release_ver']:
         if not args[arg] or (isinstance(args[arg], list) and not args[arg][0]):
             print("ERROR: %s is required" % arg)
             valid_input = False
@@ -107,40 +119,92 @@ def merge(primary, secondary):
 # run the code...
 if __name__ == '__main__':
     args = load_config()
-
-    # repository details
+#     repository details
     gh_token = args['--gh_token']
     repo_name = args['--repo']
-    prev_release_hash = args['--prev_rel_commit']
-    versions = []
-    if isinstance(args['--branches'], list):
-        versions = [x.strip() for x in args['--branches'][0].split(',')]
-    else:
-        versions = [x.strip() for x in args['--branches'].split(',')]
+    prev_release_ver = args['--prev_release_ver']
+    prev_release_commit = args['--prev_release_commit']
+    new_release_ver = args['--new_release_ver']
+    branch = args['--branch']
 
     gh_base_url = args['--gh_base_url']
     jira_base_url = args['--jira_base_url']
     jira_server_url = args['--jira_server_url']
 
-    # table column widths
+#     table column widths
     branch_len = int(args['--col_branch_width'])
     gh_len = int(args['--col_github_width'])
     jira_len = int(args['--col_jira_width'])
     issue_type_len = int(args['--col_type_width'])
     issue_priority_len = int(args['--col_priority_width'])
     desc_len = int(args['--col_desc_width'])
-
-    # connect to jira and github
-    jira = JIRA({
-        'server': jira_server_url
-    })
+    
+    outputfile = str(os.path.splitext(args['--config'])[0])+".rst"
+##
+#     connect to jira and github
+##    jira = JIRA({
+##        'server': jira_server_url
+##    })
+##    gh = Github(gh_token)
+##
+#     get the repo and commits
+##    repo = gh.get_repo(repo_name)
+##    commits = repo.get_commits()
+##
+#     loop through the commits and pull relevant PR numbers
+##    merged = []
+##    reverted = []
+##    for c in commits:
+#         break when we hit the previous release commit
+##        if c.sha == prev_release_hash:
+##            break
+##
+#         get the first line of the commit message
+##        commit_msg = c.commit.message.splitlines()[0]
+##
+#         make sure the commit is a PR merge (using the `git pr ####` tool)
+##        if 'Revert "Merge pull request #' == commit_msg[0:28]:
+#             eg: Revert "Merge pull request #1493 from shapeblue/nio-fix"
+##            pr_num = int(commit_msg[28:].split(' ')[0]) # get the text until the next space and cast to int
+##            if pr_num not in reversed:
+##                reverted.append(pr_num)
+##        if 'Merge pull request #' == commit_msg[0:20]:
+#             eg: Merge pull request #1523 from nlivens/bug/CLOUDSTACK-9365
+##            pr_num = int(commit_msg[20:].split(' ')[0]) # get the text until the next space and cast to int
+##            if pr_num not in merged:
+##                merged.append(pr_num)
+##
+#         make sure we pick up the PR merges which are done through Github
+##        regex = r"\(#(\d+)\)$"
+##        matches = re.findall(regex, commit_msg)
+##        for match in matches:
+##            if match not in merged:
+##                merged.append(int(match))
+##
+#     removed reverted PRs from the merged list
+##    merged = [pr for pr in merged if pr not in reverted]
+        
+    
     gh = Github(gh_token)
-
-    # get the repo and commits
     repo = gh.get_repo(repo_name)
-    commits = repo.get_commits()
+    repo_tags = repo.get_tags()
+    if prev_release_commit:
+        print("Previous Release Commit SHA found, overriding pre_release_ver")
+        prev_release_hash = prev_release_commit
+    else:
+        print("Finding commit SHA for previous version")
+        for tag in repo_tags:
+            if tag.name == prev_release_ver:
+                prev_release_hash = tag.commit.sha
+                print("name: %s tag.sha: %s" % (tag.name, tag.commit.sha))
 
-    # loop through the commits and pull relevant PR numbers
+    if not prev_release_hash:
+        print("No starting point found via version tag or commit SHA")
+        exit
+
+    print("Retrieving commits from %s" % branch)        
+    commits = repo.get_commits(sha=(branch))
+    
     merged = []
     reverted = []
     for c in commits:
@@ -148,38 +212,41 @@ if __name__ == '__main__':
         if c.sha == prev_release_hash:
             break
 
+        print("Adding commit %s" % c.sha)
         # get the first line of the commit message
         commit_msg = c.commit.message.splitlines()[0]
-
+    
         # make sure the commit is a PR merge (using the `git pr ####` tool)
         if 'Revert "Merge pull request #' == commit_msg[0:28]:
             # eg: Revert "Merge pull request #1493 from shapeblue/nio-fix"
             pr_num = int(commit_msg[28:].split(' ')[0]) # get the text until the next space and cast to int
-            if pr_num not in reversed:
+            if pr_num not in reverted:
                 reverted.append(pr_num)
         if 'Merge pull request #' == commit_msg[0:20]:
             # eg: Merge pull request #1523 from nlivens/bug/CLOUDSTACK-9365
             pr_num = int(commit_msg[20:].split(' ')[0]) # get the text until the next space and cast to int
             if pr_num not in merged:
                 merged.append(pr_num)
-
+    
         # make sure we pick up the PR merges which are done through Github
         regex = r"\(#(\d+)\)$"
         matches = re.findall(regex, commit_msg)
         for match in matches:
             if match not in merged:
                 merged.append(int(match))
-
+    
+    print("Removing reverted commits..")
     # removed reverted PRs from the merged list
     merged = [pr for pr in merged if pr not in reverted]
+    
+    print("Creating table..")
 
     # start building the table(s)
     table = None
     try:
         table = TableRST([
-            ('Branches', branch_len),
+            ('Version', branch_len),
             ('Github', gh_len),
-            ('Jira', jira_len),
             ('Type', issue_type_len),
             ('Priority', issue_priority_len),
             ('Description', desc_len),
@@ -188,9 +255,8 @@ if __name__ == '__main__':
         print('ERROR: %s' % str(e))
 
     md = TableMD([
-        'Branches',
+        'new_release_ver',
         'Github',
-        'Jira',
         'Type',
         'Priority',
         'Description'
@@ -211,47 +277,32 @@ if __name__ == '__main__':
         jira_ticket = ''
         jira_url = ''
 
-        # check if there is a jira ticket associated
-        jira_prefix = 'CLOUDSTACK-' # string to check for jira ticket
-        jira_issue_index = pr.title.upper().find(jira_prefix)
-        if jira_issue_index != -1: # has jira ticket
-            # parse out the jira ticket int from the text
-            jira_int = "".join(itertools.takewhile(str.isdigit, 
-                (pr.title[jira_issue_index + len(jira_prefix):]).encode('ascii', 'ignore')))
-            jira_ticket = '%s%s' % (jira_prefix, jira_int)
-            try:
-                # get the jira ticket from the api and override defaults
-                issue = jira.issue(jira_ticket, expand="names")
-                jira_url = '%s%s' % (jira_base_url, jira_ticket)
-                links.append('.. _%s: %s' % (jira_ticket, jira_url))
-                issue_type = issue.fields.issuetype.name
-                issue_priority = issue.fields.priority.name
-                desc = issue.fields.summary.strip()
-            except:
-                continue # move on if there is an issue getting the ticket
-
-        # add the branch details
-        branches = ''
-        if branch == 'master':
-            branches = versions[-1] # last item in the version list
-        else:
-            if branch in versions:
-                branches = ', '.join(versions[versions.index(branch):]) # assume fwd-merged
-            else:
-                branches = '%s*' % branch
-        
-        jira_output = ''
-        if jira_url:
-            jira_output = '%s_' % jira_ticket
-        else:
-            jira_output = jira_ticket
+#        # check if there is a jira ticket associated
+#        jira_prefix = 'CLOUDSTACK-' # string to check for jira ticket
+#        jira_issue_index = pr.title.upper().find(jira_prefix)
+#        if jira_issue_index != -1: # has jira ticket
+#            # parse out the jira ticket int from the text
+#            jira_int = "".join(itertools.takewhile(str.isdigit, 
+#                (pr.title[jira_issue_index + len(jira_prefix):]).encode('ascii', 'ignore')))
+#            jira_ticket = '%s%s' % (jira_prefix, jira_int)
+#            try:
+#                # get the jira ticket from the api and override defaults
+#                issue = jira.issue(jira_ticket, expand="names")
+#                jira_url = '%s%s' % (jira_base_url, jira_ticket)
+#                links.append('.. _%s: %s' % (jira_ticket, jira_url))
+#                issue_type = issue.fields.issuetype.name
+#                issue_priority = issue.fields.priority.name
+#                desc = issue.fields.summary.strip()
+#            except:
+#                continue # move on if there is an issue getting the ticket
+#
+#        # add the branch details
 
         if table:
             try:
                 table.add_row([
-                    branches,
+                    new_release_ver,
                     '`#%s`_' % pr_num,
-                    jira_output,
                     issue_type,
                     issue_priority,
                     desc
@@ -259,22 +310,24 @@ if __name__ == '__main__':
             except IOError as e:
                 print('ERROR: %s' % str(e))
         md.add_row([
-            branches,
+            new_release_ver,
             '[#%s](%s)' % (pr_num, gh_url),
-            ('[%s](%s)' % (jira_ticket, jira_url)) if jira_ticket and jira_url else '',
             issue_type,
             issue_priority,
             desc
         ])
     if table:
-        print('\n\n\n.. cssclass:: table-striped table-bordered table-hover\n')
-        print(table.draw())
-
+        file = open('%s.txt' % outputfile ,"w")
+        file.write('\n.. cssclass:: table-striped table-bordered table-hover\n\n\n')
+        file.write(table.draw())
+    file.write('\n%s Issues listed\n\n' % len (merged) )
+    
     # output the links we referenced earlier
     for link in links:
-        print(link)
-    print('')
+        file.write('%s \n' % link)
+    file.write('')
 
     #print(md.draw()) # draw a markdown version of table as well
-
-
+    file.close()
+    print("Commit data output to %s.txt" % outputfile)
+    
